506bb6d1d7c5a10c93cca9c2276e3dd9ca66b752
Sergey Filippov <sergey.filippov.89@inbox.ru>
2022-04-07 14:22:36 +0800

    lecture code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-const list = document.querySelector("#results")
+import { initSortable } from "./plugins/init_sortable";
+initSortable();
+
+const list = document.querySelector("#results");
 
 const insertMovies = (data) => {
   data.Search.forEach((result) => {
     const movieTag = `<li class="list-group-item border-0">
       <img src="${result.Poster}" alt="" width="100">
-    </li>`
-    list.insertAdjacentHTML("beforeend", movieTag)
-  })
-}
+    </li>`;
+    list.insertAdjacentHTML("beforeend", movieTag);
+  });
+};
 
 const fetchMovies = (query) => {
   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-    .then(response => response.json())
-    .then(insertMovies)
-}
+    .then((response) => response.json())
+    .then(insertMovies);
+};
 
-fetchMovies("harry potter") // on 1st page load
+fetchMovies("harry potter"); // on 1st page load
 
-const form = document.querySelector("#search-form")
+const form = document.querySelector("#search-form");
 form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  list.innerHTML = ""
-  const input = document.querySelector("#search-input")
-  fetchMovies(input.value)
-})
+  event.preventDefault();
+  list.innerHTML = "";
+  const input = document.querySelector("#search-input");
+  fetchMovies(input.value);
+});
